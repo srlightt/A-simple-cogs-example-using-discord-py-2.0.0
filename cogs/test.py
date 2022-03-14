f5857ddd7297e4a54532618f11d0ec48a99dffb1
PC L <lightgraphics80@gmail.com>
2022-03-14 17:32:12 -0300

    Changes to async initialisation

--- cogs/test.py
+++ cogs/test.py
@@ -3,7 +3,7 @@ from discord.ext import commands
 from discord import app_commands
 
 
-class test(commands.Cog):
+class Test(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
@@ -11,8 +11,9 @@ class test(commands.Cog):
     @app_commands.command()
     #@app_commands.guilds() If you want to define specific guilds (Currently, this is global)
     async def test(self, interaction: discord.Interaction):
+        """command description"""
         await interaction.response.send_message('cogs \o/')
 
 
-def setup(bot):
-    bot.add_cog(test(bot))
+async def setup(bot): # set async function
+    await bot.add_cog(Test(bot)) # Use await
--- main.py
+++ main.py
@@ -1,32 +1,33 @@
 import os
 import discord
+from discord import app_commands
 from discord.ext import commands
 
 
 class Bot(commands.Bot):
     def __init__(self):
-        super().__init__(command_prefix=".")
+        super().__init__(command_prefix="")
+
+    async def startup(self):
+        await bot.wait_until_ready()
+        await bot.tree.sync()  # If you want to define specific guilds, pass a discord object with id (Currently, this is global)
+        print('Sucessfully synced applications commands')
+        print(f'Connected as {bot.user}')
+
+    async def setup_hook(self):
+        for filename in os.listdir("./cogs"):
+            if filename.endswith(".py"):
+                try:
+                    await bot.load_extension(f"cogs.{filename[:-3]}")
+                    print(f"Loaded {filename}")
+                except Exception as e:
+                    print(f"Failed to load {filename}")
+                    print(f"[ERROR] {e}")
+
+        self.loop.create_task(self.startup())
 
 
 bot = Bot()
 
-for filename in os.listdir("./cogs"):
-    if filename.endswith(".py"):
-        try:
-            bot.load_extension(f"cogs.{filename[:-3]}")
-            print(f"Loaded {filename}")
-        except Exception as e:
-            print(f"Failed to load {filename}")
-            print(f"[ERROR] {e}")
-
-
-async def startup():
-    await bot.wait_until_ready()
-    await bot.tree.sync() # If you want to define specific guilds, pass a discord object with id (Currently, this is global)
-    print('Sucessfully synced applications commands')
-    print(f'Connected as {bot.user}')
-
-
-bot.loop.create_task(startup())
 
 bot.run('token')
